dfbbeb45082528333d63963ef1e88852e3062a0c
oushihao <oushihao97@163.com>
2021-12-19 14:38:08 +0800

    style: 修改项目结构

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from '../../lib/vue-router'
+import Home from '../views/Home.vue'
+
+/*
+* VueRouter是一个插件
+*   1）实现并声明两个组件router-view router-link
+*   2）install: this.$router.push()
+* */
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        children: [
+            {
+                path: '/about/info',
+                component: {
+                    render(h) {
+                        return h('div', 'info page')
+                    }
+                }
+            }
+        ]
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
